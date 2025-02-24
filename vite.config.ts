f2aad45903694943e0c798e8f8c93360bc932834
pinomaker-hoo <inhoo23@naver.com>
2025-02-25 01:43:32 +0900

    feat : Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,46 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const __dirname = process.cwd()
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Job Score',
+        short_name: 'Job Score',
+        description:
+          process.env.VITE_APP_DESCRIPTION ||
+          '우리 회사의 잡스코어는? 우리회사와 내가 원하는 회사의 점수를 비교해보세요!',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/pwa-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}'],
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+      },
+      injectManifest: {
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
